8cbdfc22237d5ccfa31baa0e448d1aa9609be289
Curtis Sand <curtis@curtissand.com>
2019-05-23 13:57:36 -0700

    js: add conditional loading for projects and posts pages

--- src/js/curtissand.js
+++ src/js/curtissand.js
@@ -12,6 +12,11 @@ export function load() {
   if(window.location.href.indexOf('about.html') > -1){
     console.log('loading about page.');
     $('#bio_content').load('./sources/about_body.html')
+  } else if(window.location.href.indexOf('projects.html') > -1){
+    console.log('loading projects page.');
+    $('#projects').load('./sources/projects_body.html')
+  } else if(window.location.href.indexOf('posts.html') > -1){
+    console.log('loading posts page.');
   }
   console.log('javascript loaded.');
 }
