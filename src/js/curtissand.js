e7d4c59bc2a8348f12fef976ae51d6ad1cffd49c
Curtis Sand <curtis@curtissand.com>
2019-06-04 20:47:44 -0700

    js: update load URLs for projects.html and about.html

--- src/js/curtissand.js
+++ src/js/curtissand.js
@@ -13,10 +13,10 @@ import {PostsPager} from "./posts-pager.js";
 export function load() {
   if(window.location.href.indexOf('about.html') > -1){
     console.log('loading about page.');
-    $('#bio_content').load('./sources/about_body.html')
+    $('#bio_content').load('./sources/about.html')
   } else if(window.location.href.indexOf('projects.html') > -1){
     console.log('loading projects page.');
-    $('#projects').load('./sources/projects_body.html')
+    $('#projects').load('./sources/projects.html')
   } else if(window.location.href.indexOf('posts.html') > -1){
     console.log('loading posts page.');
     var posts_pager = new PostsPager('./sources/metadata.json');
