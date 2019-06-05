f2c1fdc18b85f680607670f9dc40c99f5681ea50
Curtis Sand <curtis@curtissand.com>
2019-06-04 20:50:20 -0700

    pages: add a host variable to the base template
    The host variable helps eliminate use of relative URLs to the other
    pages on the site.

--- src/skabelon/static_pages.py
+++ src/skabelon/static_pages.py
@@ -3,21 +3,26 @@ from pathlib import Path
 from time import strftime
 
 def dispatch(**kwargs):
+    if 'host' not in kwargs:
+        raise Exception('posts.py dispatch script requires a "host" '
+                        'dispatch option.')
+    host = kwargs['host']
     now = strftime('%Y-%m-%d %H:%M')
 
     dist = Path('./dist')
     jobs = [
         ('index.html',
-         {'title': 'Home', 'date': now, 'active_nav': 'index'},
+         {'title': 'Home', 'date': now, 'active_nav': 'index', 'host': host},
          dist.joinpath('index.html')),
         ('projects.html',
-         {'title': 'Projects', 'date': now, 'active_nav': 'projects'},
+         {'title': 'Projects', 'date': now, 'active_nav': 'projects',
+          'host': host},
          dist.joinpath('projects.html')),
         ('posts.html',
-         {'title': 'Posts', 'date': now, 'active_nav': 'posts'},
+         {'title': 'Posts', 'date': now, 'active_nav': 'posts', 'host': host},
          dist.joinpath('posts.html')),
         ('about.html',
-         {'title': 'About', 'date': now, 'active_nav': 'about'},
+         {'title': 'About', 'date': now, 'active_nav': 'about', 'host': host},
          dist.joinpath('about.html'))
     ]
 
